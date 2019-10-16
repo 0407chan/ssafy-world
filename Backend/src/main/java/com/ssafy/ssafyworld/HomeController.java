b832f1973a3c41d0f735f9cd774cfc45c1ee1356
최재형 <jae637@naver.com>
2019-10-16 11:10:14 +0900

    back환경 통일화

--- Backend/src/main/java/com/ssafy/ssafyworld/HomeController.java
+++ Backend/src/main/java/com/ssafy/ssafyworld/HomeController.java
@@ -1,15 +1,11 @@
 package com.ssafy.ssafyworld;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
