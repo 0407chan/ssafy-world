4175b76f10574b56d87588cda12a2da60f8df953
Junboom <ticktock0414@gmail.com>
2019-10-17 15:40:11 +0900

    CREATE chat environment

---
+++ Frontend/src/components/contents/BeforeLoginContent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <v-img
+            :src="require('@/assets/jaws.jpg')"
+            class="my-3"
+            contain
+        ></v-img>
+        <v-btn v-on:click="test"></v-btn>
+    </div>
+</template>
+
+<script>
+import api from '@/api'
+import { mapState, mapActions } from "vuex";
+
+export default {
+    name: 'BeforeLoginContent',
+    components: {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      ...mapActions("data", ['getUser']),
+      test() {
+        console.log("해보자")
+        var params = {
+          id: 0,
+          name: "이찬호",
+        }
+        this.getUser(params);
+      }
+    }
+};
+</script>
---
+++ Frontend/src/components/contents/GroupChatRoom.vue
@@ -0,0 +1,31 @@
+<template>
+    
+</template>
+
+<script>
+import api from '@/api'
+import { mapState, mapActions } from "vuex";
+
+export default {
+    name: 'GroupChatRoom',
+    components: {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      ...mapActions("data", ['getUser']),
+      test() {
+        console.log("해보자")
+        var params = {
+          id: 0,
+          name: "이찬호",
+        }
+        this.getUser(params);
+      }
+    }
+};
+</script>
