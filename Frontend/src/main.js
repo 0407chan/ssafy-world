3dcfdca38ed34936cf577fbb6843b418c00b2b20
최재형 <jae637@naver.com>
2019-10-23 11:54:30 +0900

    node와 프론트 연동

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import socketPlugin from './plugins/socketPlugin';
 import router from './router/router'
 import store from './store'
-import './plugins/socketPlugin';
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
@@ -11,6 +11,7 @@ Vue.config.devtools = true
 new Vue({
   el: '#app',
   vuetify,
+  socketPlugin,
   render: h => h(App),
   router,
   store,
