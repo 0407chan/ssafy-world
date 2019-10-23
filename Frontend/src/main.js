b9ebdc45fb1f8eab0aaecc6f37df46440a954a1e
이찬호 <0407chan@naver.com>
2019-10-23 16:33:18 +0900

    Checkout

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import socketPlugin from './plugins/socketPlugin';
+import './plugins/socketPlugin';
 import router from './router/router'
 import store from './store'
 
@@ -11,7 +11,6 @@ Vue.config.devtools = true
 new Vue({
   el: '#app',
   vuetify,
-  socketPlugin,
   render: h => h(App),
   router,
   store,
