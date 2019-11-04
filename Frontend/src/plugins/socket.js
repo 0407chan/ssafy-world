50d6c306083bf86ef2565dfa7106182ba0314efb
최재형 <jae637@naver.com>
2019-11-04 10:29:22 +0900

    Merging

--- Frontend/src/plugins/socket.js
+++ Frontend/src/plugins/socket.js
@@ -13,7 +13,7 @@ const SocketPlugin = {
       // socket.emit('/chatroom'+$payload.rid, {
       socket.emit($payload.rid, {
         msg: $payload.msg,
-        name: $payload.name,
+        user: $payload.user,
         time: $payload.time,
       });
     };
@@ -23,4 +23,4 @@ const SocketPlugin = {
   },
 };
 
-//Vue.use(SocketPlugin);
+Vue.use(SocketPlugin);
